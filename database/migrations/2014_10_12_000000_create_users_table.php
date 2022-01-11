5e1cea734ffd3d7d20b6d0086ec009da527d10f6
Ikramul Islam Emon <72353467+ikramul-cseju@users.noreply.github.com>
2022-01-12 01:18:45 +0600

    migration file for register update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,15 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('user_type');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('gender');
+            $table->string('institution');
+            $table->string('contact_no');
+            $table->string('address');
+
             $table->rememberToken();
             $table->timestamps();
         });
